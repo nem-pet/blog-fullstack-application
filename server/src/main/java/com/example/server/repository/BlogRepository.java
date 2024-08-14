eba8e283dc978ce9bbf66479c163b7b239d99fd8
Nemanja Petrušić <89937019+nem-pet@users.noreply.github.com>
2024-08-14 15:42:39 +0200

    Delete server directory

--- server/src/main/java/com/example/server/repository/BlogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.server.repository;
-
-import com.example.server.model.Blog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlogRepository extends JpaRepository<Blog, Integer> {
-}
